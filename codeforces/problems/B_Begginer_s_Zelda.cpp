3557ee9cf40adec9a2475e372231672b1517ce24
Plasmaa <zarifpious11@gmail.com>
2024-10-03 08:12:18 +0600

    leetcode dp

---
+++ codeforces/problems/B_Begginer_s_Zelda.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#define no cout << "NO" << '\n'
+#define yes cout << "YES" << '\n'
+#define all(x) x.begin(), x.end()
+#define fast() ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+typedef long long int ll;
+using namespace std;
+
+int main()
+{
+    fast();
+    
+    return 0;
+}
